5b9544b461a3816bb03674d8acfa4912853a345c
Joe Ellingworth <joeellingworth@Joes-MacBook-Pro-2.local>
2024-09-17 11:15:27 +0100

    create with balance test passing

--- bank_account.py
+++ bank_account.py
@@ -1,4 +1,7 @@
 class Bank_account:
     __account_name = ""
-    def __init__(self, account_name):
+    __balance = 0
+
+    def __init__(self, account_name, balance):
         self.account_name = account_name
+        self.balance = balance
--- test_bank_account.py
+++ test_bank_account.py
@@ -1,6 +1,10 @@
 from bank_account import Bank_account
 
-def test_bank_account_init_has_amout():
-    customer1 = Bank_account("Test customer")
+def test_bank_account_init_has_name():
+    customer1 = Bank_account("Test customer", 10)
 
     assert customer1.account_name == "Test customer"
+
+def test_account_has_balance():
+    customer2 = Bank_account("Test Test", 10)
+    assert customer2.balance == 10
