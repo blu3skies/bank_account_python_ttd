720bd578528049fa3a8ad2799fc6163ce55ae9f1
Joe Ellingworth <joeellingworth@Joes-MacBook-Pro-2.local>
2024-09-17 10:10:36 +0100

    first passing test, account has a name

---
+++ bank_account.py
@@ -0,0 +1,4 @@
+class Bank_account:
+    __account_name = ""
+    def __init__(self, account_name):
+        self.account_name = account_name
--- test_bank_account.py
+++ test_bank_account.py
@@ -1,2 +1,6 @@
-from bank_account import __init__
+from bank_account import Bank_account
 
+def test_bank_account_init_has_amout():
+    customer1 = Bank_account("Test customer")
+
+    assert customer1.account_name == "Test customer"
