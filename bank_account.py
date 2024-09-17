0e60669983545c1e94bbb21ae7a5f1f164445878
Joe Ellingworth <joeellingworth@Joes-MacBook-Pro-2.local>
2024-09-17 11:38:19 +0100

    added data checks tests to account name and balance

--- bank_account.py
+++ bank_account.py
@@ -5,3 +5,10 @@ class Bank_account:
     def __init__(self, account_name, balance):
         self.account_name = account_name
         self.balance = balance
+
+#customer1 = Bank_account("Joe e", 100)
+customer2 = Bank_account(10, "bobby")
+
+isstring = isinstance(customer2.balance, (float, int)) != False
+
+print(isstring)
--- test_bank_account.py
+++ test_bank_account.py
@@ -1,10 +1,17 @@
 from bank_account import Bank_account
 
-def test_bank_account_init_has_name():
-    customer1 = Bank_account("Test customer", 10)
+customer1 = Bank_account("Test customer", 10)
+customer2 = Bank_account(10, "bobby")
 
+def test_bank_account_init_has_name():
     assert customer1.account_name == "Test customer"
 
+def test_bank_account_name_is_string():
+    assert isinstance(customer1.account_name,str)
+    assert isinstance(customer2.account_name,str) == False
 def test_account_has_balance():
-    customer2 = Bank_account("Test Test", 10)
-    assert customer2.balance == 10
+    assert customer1.balance == 10
+
+def test_account_balance_is_float_or_int():
+    assert isinstance(customer1.balance, (float, int))
+    assert isinstance(customer2.balance, (float, int)) == False
